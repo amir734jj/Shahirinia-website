e70e6cbc5f3c8aebcbac2ad52f9789b21de8ae57
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2021-11-03 11:14:32 -0500

    started the work

---
+++ server.js
@@ -0,0 +1,16 @@
+// Install express server
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Use CORS
+app.use(cors());
+
+// Serve only the static files form the dist directory
+app.use(express.static('src'));
+
+// Start the app by listening on the default Heroku port
+const server = app.listen(process.env.PORT || 80, function() {
+    console.log('Server is running at %s:%d', server.address().address, server.address().port);
+});
